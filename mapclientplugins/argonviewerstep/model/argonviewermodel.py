c6aace542cea1cce43db2da3bff13382c71b5cff
Kay <kayvvw@gmail.com>
2021-07-21 11:00:34 +1200

    Export WebGL Json

--- mapclientplugins/argonviewerstep/model/argonviewermodel.py
+++ mapclientplugins/argonviewerstep/model/argonviewermodel.py
@@ -13,6 +13,7 @@ from opencmiss.zinc.material import Material
 from opencmiss.zinc.node import Node
 from opencmiss.zinc.scenecoordinatesystem import SCENECOORDINATESYSTEM_NORMALISED_WINDOW_FIT_LEFT
 from opencmiss.zinc.scenefilter import Scenefilter
+from opencmiss.zinc.streamscene import StreaminformationScene 
 from opencmiss.zinc.result import RESULT_OK
 from opencmiss.argon.core.argondocument import ArgonDocument
 from opencmiss.argon.core.argonlogger import ArgonLogger
@@ -32,6 +33,7 @@ class ArgonViewerModel(object):
         # self._initGraphicsModules()
         self._document = ArgonDocument()
         self._document.initialiseVisualisationContents()
+        self._prefix = "Argon"
         self.load(inputArgonDocFile)
 
     def load(self, filename):
@@ -98,7 +100,10 @@ class ArgonViewerModel(object):
         with open(self._location, 'w') as f:
             f.write(state)
 
-    def done(self):
+    def done(self,view):
+        print("create graphics")
+        self.exportViewJson(view)
+        self.exportWebGLJson()
         self.save()
 
     def getIdentifier(self):
@@ -113,7 +118,86 @@ class ArgonViewerModel(object):
         graphics.setVisibilityFlag(show)
 
     def createGraphics(self):
-        pass
+        print("create graphics",self._document.getRootRegion().getZincRegion().getScene())
+        streaminformationScene = self.getScene().createStreaminformationScene()
+        print("create graphics",streaminformationScene)
+
+        streaminformationScene.setIOFormat(StreaminformationScene.IO_FORMAT_THREEJS)
+        streaminformationScene.createStreamresourceFile("threejs.json")
+        
+        self.getScene().write(streaminformationScene)
+        print(streaminformationScene)
 
     def getScene(self):
-        pass
+        print("create graphics scene")
+        return self._document.getRootRegion().getZincRegion().getScene()
+        
+    def exportViewJson(self,view):
+        '''Export sceneviewer parameters to JSON format'''
+        sceneviewerState = self.getSceneviewerState(view)
+        viewData = {}
+        viewData['farPlane'] = sceneviewerState["FarClippingPlane"]
+        viewData['nearPlane'] = sceneviewerState["NearClippingPlane"]
+        viewData['eyePosition'] = sceneviewerState["EyePosition"]
+        viewData['targetPosition'] = sceneviewerState["LookatPosition"]
+        viewData['upVector'] = sceneviewerState["UpVector"]
+        f = open(self._prefix + '_view' + '.json', 'w+')
+        json.dump(viewData, f)
+        f.close()
+
+    def exportWebGLJson(self):
+        '''
+        Export graphics into JSON format, one json export represents one
+        surface graphics.
+        '''
+        scene = self.getScene()
+        sceneSR = scene.createStreaminformationScene()
+        sceneSR.setIOFormat(sceneSR.IO_FORMAT_THREEJS)
+        '''
+        output frames of the deforming heart between time 0 to 1,
+        this matches the number of frame we have read in previously
+        '''
+        # sceneSR.setNumberOfTimeSteps(self._numberOfTimeSteps)
+        sceneSR.setNumberOfTimeSteps(10)
+        sceneSR.setInitialTime(0.0)
+        sceneSR.setFinishTime(1.0)
+        ''' we want the geometries and colours change overtime '''
+        sceneSR.setOutputTimeDependentVertices(1)
+        sceneSR.setOutputTimeDependentColours(1)
+        number = sceneSR.getNumberOfResourcesRequired()
+        resources = []
+        '''Write out each graphics into a json file which can be rendered with ZincJS'''
+        for i in range(number):
+            resources.append(sceneSR.createStreamresourceMemory())
+        scene.write(sceneSR)
+        '''Write out each resource into their own file'''
+        for i in range(number):
+            f = None
+            if i == 0:
+                f = open(self._prefix + '_' + 'metadata.json', 'w+')
+            else:
+                f = open(self._prefix + '_' + str(i) + '.json', 'w+')
+            buffer = resources[i].getBuffer()[1].decode()
+            
+            if i == 0:
+                for j in range(number-1):
+                    '''
+                    IMPORTANT: the replace name here is relative to your html page, so adjust it
+                    accordingly.
+                    '''
+                    # viewData = "{\"Type\":\"View\", \n \"URL\" : %s}"%(self._prefix + '_view' + '.json')
+                    # buffer += viewData
+                    replaceName = '' + self._prefix + '_' + str(j+1) + '.json'
+                    old_name = 'memory_resource'+ '_' + str(j+2)
+                    print(buffer)
+                    print(type(buffer))
+                    buffer = buffer.replace(old_name, replaceName)
+                viewObj =    {
+	                "Type": "View",
+	                "URL": self._prefix + '_view' + '.json'
+                }
+                obj = json.loads(buffer)
+                obj.append(viewObj)
+                buffer = json.dumps(obj)
+            f.write(buffer)
+            f.close()    
