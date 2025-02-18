c210f5b0028cc65a390433f9ec3eecea4a2955d5
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-02-18 14:19:41 +1300

    Remove .git from __location__.

--- mapclientplugins/argonviewerstep/__init__.py
+++ mapclientplugins/argonviewerstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.4.7'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'ArgonViewer'
-__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.argonviewerstep.git'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.argonviewerstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.argonviewerstep import step
